da4bd7b70328ac34e16e2e009241cbb4b194a3f5
Tzekov <stanislav.tzekov@dxc.com>
2024-12-10 17:01:52 +0200

    Add table with consumed food

--- myapp/views.py
+++ myapp/views.py
@@ -7,13 +7,30 @@ def index(request):
 
     if request.method == "POST":
         selected_food = request.POST.get("foods")
+
         food = Food.objects.get(name=selected_food)
         user = request.user
+
         consume = Consume(consumed_food=food, user=user)
         consume.save()
 
+    user_consumed_food = request.user.consumes.all()
+
+    for i in range(len(user_consumed_food)):
+        user_consumed_food[i].curr_number = i+1
+
+    total_carbs = round(sum(f.consumed_food.carbs for f in user_consumed_food), 2)
+    total_proteins = round(sum(f.consumed_food.proteins for f in user_consumed_food), 2)
+    total_fats = round(sum(f.consumed_food.fats for f in user_consumed_food), 2)
+    total_calories = sum(f.consumed_food.calories for f in user_consumed_food)
+
     context = {
-        "foods": foods
+        "foods": foods,
+        "consumes": user_consumed_food,
+        "total_carbs": total_carbs,
+        "total_proteins": total_proteins,
+        "total_fats": total_fats,
+        "total_calories": total_calories
     }
 
     return render(request, "myapp/index.html", context)
