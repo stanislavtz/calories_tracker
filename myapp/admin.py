090fa20a7d180637b13f3c82ba22c84c98da75f0
Tzekov <stanislav.tzekov@dxc.com>
2024-12-10 09:52:57 +0200

    Add Food model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Food
 
 # Register your models here.
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ("name", "id", "carbs", "proteins", "fats", "calories")
+    list_filter = ("carbs", "proteins", "fats", "calories")
+
+
+admin.site.register(Food, FoodAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    calories = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.name
