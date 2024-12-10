1a248333f6cc641eda105292901172902b6a3c4a
Tzekov <stanislav.tzekov@dxc.com>
2024-12-10 15:22:56 +0200

    Add functionality user to add food

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
 
 # Create your views here.
 def index(request):
     foods = Food.objects.all()
-    return render(request, "myapp/index.html", {"foods": foods})
+
+    if request.method == "POST":
+        selected_food = request.POST.get("foods")
+        food = Food.objects.get(name=selected_food)
+        user = request.user
+        consume = Consume(consumed_food=food, user=user)
+        consume.save()
+
+    context = {
+        "foods": foods
+    }
+
+    return render(request, "myapp/index.html", context)
